f81912737e04755a11a779fe221476a6b1766d4e
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-01-03 13:51:00 +0300

    .

---
+++ client/src/components/admin/Login.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg">
+        <div className="flex flex-col items-center justify-center">
+          <div className="w-full py-6 text-center">
+            <h1 className="text-3xl font-bold">
+              <span className="text-primary">Admin</span> Giriş
+            </h1>
+            <p className="font-light">
+              Yönetici paneline erişmek için giriş bilgilerinizi girin.
+            </p>
+          </div>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col">
+                <label>Email</label>
+                <input
+                  type="email"
+                  required
+                  placeholder="Emailinizi Giriniz"
+                  className="border-b-2 border-gray-300 p-2 outline-none mb-6"
+                />
+              </div>
+              <div className="flex flex-col">
+                <label>Şifre</label>
+                <input
+                  type="password"
+                  required
+                  placeholder="Şifrenizi Giriniz"
+                  className="border-b-2 border-gray-300 p-2 outline-none mb-6"
+                />
+              </div>
+              <button
+                className="w-full py-3 font-medium bg-primary text-white rounded cursor-poimter hover:bg-primary/90 transition-all cursor-pointer"
+                type="submit"
+              >
+                Giriş
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
